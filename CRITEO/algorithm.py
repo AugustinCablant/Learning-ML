ca3d780ecf55f0370d509312e978f8592dd63bbb
Augustin Cablant <augustin.cablant@ensae.fr>
2024-11-25 08:47:21 +0100

    up

--- CRITEO/algorithm.py
+++ CRITEO/algorithm.py
@@ -1,75 +1,9 @@
 import numpy as np 
 from Tree import Tree
 
-class BlueBusRedBusEnvironment:
-    def __init__(self, s):
-        """ 
-        Params:
-        ------
-        s: int, size of the Structure
-        """
-        self.name = 'BlueBusRedBus'
-        self.s = s
-        self.tree = Tree()
-        self.root = self.tree.add_node(None, ('root', 0))
-
-    def set(self):
-        """
-        Create the Tree and find the best path
-        """
-        self.create_graph()
-        _, self.best_strategy_path = self.tree.find_best_arm_path()
-
-        # Construit le chemin optimal à partir des indices
-        nodes_path = [self.root]
-        node = self.root
-        for idx in self.best_strategy_path:
-            node = node.children[idx]
-            nodes_path.append(node)
-        self.best_strategy_nodes_path = nodes_path
-
-    def create_graph(self):
-        """
-        Crée le graphe de l'environnement (arbre) avec un nombre spécifique d'enfants par noeud.
-        """
-        self._create_sub_level('root', 2)  # Crée les enfants de la racine
-        node_key = 'level_1_child_number_1_of_root'  # Exemple de clé pour l'un des enfants
-        self._create_sub_level(node_key, self.nb_assymetric_child)  # Crée des enfants asymétriques
-
-    def _create_sub_level(self, node_key, nb_childs):
-        """
-        Crée un sous-niveau (enfants) pour un noeud donné.
-        :param node_key: Clé du noeud pour lequel créer des enfants.
-        :param nb_childs: Nombre d'enfants à créer pour ce noeud.
-        """
-        node = self.tree.graph[node_key]
-        level = node.level + 1
-        for n in range(nb_childs):
-            name = f'level_{level}_child_number_{n}_of_{node.name}'
-            value = self.env_rng.choice(range(1, 9))  # Tirage aléatoire pour la valeur
-            self.tree.insert(node, (name, value))
-
-    def get_reward_by_path(self, path):
-        """
-        Calcule la récompense pour un chemin donné.
-        :param path: Liste des noeuds dans le chemin.
-        :return: La somme des récompenses sur ce chemin.
-        """
-        reward = 0
-        for node in path[1:]:  # On ignore la racine (premier noeud)
-            level_correction = 10 ** (-node.level)
-            reward += 0.1 * level_correction * node.value
-        return reward
-
-    def get_best_strategy_reward(self):
-        """
-        Retourne la récompense du meilleur chemin stratégique trouvé.
-        """
-        return self.get_reward_by_path(self.best_strategy_nodes_path)
-
 
 class NEW:
-    def __init__(self, K, s, Niter):
+    def __init__(self, K, s, Niter, eps = 10 ** (-8)):
         """ 
         Args:
             K: int, number of arms
@@ -80,3 +14,42 @@ class NEW:
         self.K = K
         self.s = s
         self.Niter = Niter
+        self.eps = eps
+
+
+    def set_environment(self, environment):
+        self.environment = environment
+
+    def vector_proba(self, scores):
+        stable_exp_y = np.exp(scores - np.max(scores))
+        proba_vector = stable_exp_y/np.sum(stable_exp_y)
+        return proba_vector
+
+    def sample_node_path(self, round):
+        node_path = []
+        proba_path = []
+        reward_path = []
+        node = self.environment.tree.root
+
+        while bool(node.children):   # tant que le noeud a des enfants 
+            delta = 1 / np.sqrt(round + 1)    # facteur d'exploration
+            proba = self.vector_proba(node.scores_children * delta)
+            idx_list = range(node.nb_children)
+            idx_node = self.rng.choice(idx_list, p=proba)
+            child_node = node.children[idx_node]
+            node_path.append(idx_node)
+            reward_child = self.environment.get_reward_by_node(child_node)
+            reward_path.append(reward_child)
+            proba_path.append(proba[idx_node])
+            node = child_node
+
+        return node_path, proba_path, reward_path
+
+    def scoring_phase(self, tree, )
+        
+    
+    def learn(self):
+        regrets, rewards = np.zeros(self.Niter), np.zeros(self.Niter)
+
+        for t in range(self.Niter):
+            
